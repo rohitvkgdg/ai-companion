f4a73d8197842bdc9eedda88f46874640ea526fa
Rohit <rohitvkgdg@gmail.com>
2024-10-17 00:30:50 +0530

    "Updated Footer, Navbar, and MarketingLayout components to use dark mode classes"

--- app/(marketing)/_components/Navbar.tsx
+++ app/(marketing)/_components/Navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 
     return ( 
         <div className={cn(
-            "z-50 bg-background dark:bg-[rgb(31,31,31)] fixed top-0 flex items-center w-full p-6 pb-4 pt-4", scrolled && "border-b shadow-m"
+            "z-50 bg-background dark:bg-[rgb(31,31,31)] fixed top-0 flex items-center w-full p-6 pb-4 pt-4", scrolled && "border-b border-b-slate-500 shadow-m"
         )}>
             <Logo />
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
